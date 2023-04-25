b456d738fa43d7092d0d8338d5922b8ad281a155
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-04-25 11:33:45 -0400

    Day 1 project: create band name generator

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Hello, programmer! Let's create your band name!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What was the name of your first pet?\n")
+print("This is your band name: " + city + " " + pet)
