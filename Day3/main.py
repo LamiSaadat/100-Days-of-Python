a2f88998962e4ce2b00be68895831cda4557621f
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-04-26 11:52:17 -0400

    Day3 project: create treasure island game

---
+++ Day3/main.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("You are at a cross roads. Which way do you want to go? Type 'left' or 'right'\n")
+
+if direction == "left":
+    swim_or_wait = input("You are at a lake. Would you like to swim or wait for a boat? To wait, type 'wait'. To swim, type 'swim'\n")
+    if swim_or_wait == "wait":
+        door = input("You are safely at the island! There are three doors: red, blue and yellow. Which door do you choose? Type 'red' or 'blue' or 'yellow'\n")
+        if door == 'red':
+            print("You've been burned by fire! Game over.")
+        elif door == 'blue':
+             print("You've been eaten by beasts! Game over.")
+        elif door == 'yellow':
+            print("You've found the treasure! You win!")
+        else: 
+            "Game over."
+    else:
+        print("You've been attacked by a trout! Game over!")
+else:
+    print("You've fallen in to a hole! Game over!")
